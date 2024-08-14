40516a549b603c295e5cea39269001302f9fd7e1
Nicolás Jurado <99767358+NicholashiG@users.noreply.github.com>
2024-08-14 17:38:31 -0500

    Metodo Dec a Bin hecho

--- Laboratorio1.java
+++ Laboratorio1.java
@@ -1,6 +1,19 @@
 public class Laboratorio1 {
     public static void main(String[] args) {
+        System.out.println(convertirADecimal(10, 5));
+    }
 
+    public static String convertirADecimal(int numeroDecimal, int anchoBits) {
+            String binario = Integer.toBinaryString(numeroDecimal);
+            // Asegura que el resultado tenga el ancho especificado
+            while (binario.length() < anchoBits) {
+                binario = "0" + binario;
+            }
+            // Si el binario excede el ancho, se cortará
+            if (binario.length() > anchoBits) {
+                binario = binario.substring(binario.length() - anchoBits);
+            }
+            return binario;
     }
 
 
